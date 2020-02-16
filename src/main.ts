9e6ec205825e4de58fa645ec9110bb89a14f70b5
Ayubur <ayubur.1994@gmail.com>
2020-02-16 13:08:30 +0600

    Set up and configure database

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const PORT = process.env.PORT || 3090;
+  app.listen(PORT, function(){
+     console.log(`Server running at port ${PORT}`);
+  });
 }
 bootstrap();
